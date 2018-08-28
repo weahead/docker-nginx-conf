383a51bf1d8a4959b5b954dc67f8c3daeb695b1d
Michael Zetterberg fd. Lopez <michael@weahead.se>
2018-08-28 14:27:17 +0200

    Bump to 2.0.6

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 NAME=nginx
 
 # Enable when Docker Hub supports build args.
-# docker build --build-arg name=${NAME} -t weahead/${NAME}-conf:2.0.5 .
-docker build -t weahead/${NAME}-conf:2.0.5 .
+# docker build --build-arg name=${NAME} -t weahead/${NAME}-conf:2.0.6 .
+docker build -t weahead/${NAME}-conf:2.0.6 .
